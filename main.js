da2c3348563196157b732218e69d7d064c568c4c
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2024-01-30 23:30:43 +0200

    Fifth commit

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const express = require("express"),
   User = require("./models/user");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/confetti_cuisine",
+  process.env.MONGODB_URI || "mongodb://127.00:27017/confetti_cuisine",
   { useNewUrlParser: true, useFindAndModify: false }
 );
 mongoose.set("useCreateIndex", true);
--- seed.js
+++ seed.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose"),
   Course = require("./models/course");
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/confetti_cuisine",
+  process.env.MONGODB_URI || "mongodb://127.00:27017/confetti_cuisine",
   { useNewUrlParser: true, useFindAndModify: false }
 );
 
